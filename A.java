978ba504d2e99617e1a0a3c1d348c3aa48f86c0c
Your Name <sadaqunps@gmail.com>
2024-05-27 19:25:44 +0530

    tst

---
+++ A.java
@@ -0,0 +1,22 @@
+package pack1;
+
+public class A{
+    
+
+   public void  m1(){
+    
+    System.out.println("A - Class Public - m1 method public");
+   }
+           void m2(){
+            System.out.println("A - Class Defualt - m2 method defualt");
+           }
+         protected  void m3(){
+          System.out.println("A - Class Protected - m1 method protected");
+         }
+         private void m4 (){
+          System.out.println("A - Class Privte - m1 method private");
+         }
+   
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,18 @@
+package pack1;
+//import pack1.A;
+
+public class Test{
+
+    public static void main(String[] args) {
+        
+         A a1=new A();
+
+         a1.m1();
+        // a1.m2();
+        // a1.m3();
+         //a1.m4();
+    }
+
+}
+
+
